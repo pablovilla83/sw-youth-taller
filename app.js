f681a1d9e6612f72bc7e19ab63a965683d77688e
Pablo Perez <pablo@metixmedical.co.uk>
2017-11-18 12:01:13 -0600

    making the template non npm-dependant

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 $('.btn-register').on('click', function () {
   var targetOffset = $('section.registro').offset().top
-  animatedScrollTo(document.body, targetOffset, 600)
+  $(document.body).scrollTo(targetOffset, 600);
 })
